2e5a41130467d39c69914aa015fcea7abfa05806
Elian <eliantito234@gmail.com>
2023-06-13 10:06:42 -0400

    Ejercicio 2

--- src/sumador.js
+++ src/sumador.js
@@ -1,11 +1,38 @@
 function contarPalabra(a) {
   let lista = [];
+  let control = 0;
+  let palabra = "";
+  let recorrido = 0;
+  for(let i = 0; i < a.length; i ++){
+    if(a[i] == " " || a[i] == "-" || a[i] == ";" || a[i] == "," || a[i] == "."){
+      control = i - 1;
+      while(recorrido <= control){
+        palabra += a[recorrido];
+        recorrido ++;
+      }
+      recorrido = i + 1;
+      lista.push(palabra);
+    }
+    else{
+      while(recorrido <= i){
+        palabra += a[recorrido];
+        recorrido ++;
+      }
+      recorrido = i;
+      lista.push(palabra);
+    }
+  }
+  
   lista.push(a);
   let mensaje = "";
-  if(lista.length == 1){
-    mensaje = lista[0] + ": " + 1;
-    return mensaje;
-  }
+  for(let i = 0; i < lista.length; i += 1){
+    mensaje = lista[i] + ": " + 1;
+    if(lista.length > 1){
+      mensaje += '\n';
+    }
+  } 
+  return mensaje;
 }
 
 export default contarPalabra;
+
--- src/sumador.test.js
+++ src/sumador.test.js
@@ -4,4 +4,8 @@ describe("Contar Palabra", () => {
   it("Deberia devolverme la frase y la cantidad 1", () => {
     expect(contarPalabra("bola")).toEqual("bola: 1");
   });
+  it("Deberia devolverme las frases y su cantidad 1", () => {
+    let mensaje = "bola: 1" +'\n' + "hola: 1"
+    expect(contarPalabra("bola hola")).toEqual(mensaje);
+  });
 });
