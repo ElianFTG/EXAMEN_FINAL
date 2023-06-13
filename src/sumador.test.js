2520730674c486cab9875ec6ff73a418ad0e3227
Elian <eliantito234@gmail.com>
2023-06-13 10:19:35 -0400

    Refactory de la primera prueba

--- src/sumador.test.js
+++ src/sumador.test.js
@@ -2,11 +2,10 @@ import contarPalabra from "./sumador.js";
 import verificacion from "./separadores.js";
 describe("Contar Palabra", () => {
   it("Deberia devolverme la frase y la cantidad 1", () => {
-    expect(contarPalabra("bola")).toEqual("bola: 1");
+    expect(contarPalabra("bola")).toEqual("bola: 1\n");
   });
   it("Deberia devolverme las frases y su cantidad 1", () => {
-    let mensaje = "bola: 1" + '\n' + "hola: 1";
-    expect(contarPalabra("bola hola")).toEqual(mensaje);
+    expect(contarPalabra("bola hola")).toEqual("bola: 1\nhola: 1");
   });
 });
 
